26dd2038cf620047b734a451b92d27bb448f715a
yuki saito <ykst0622@gmail.com>
2022-06-22 17:12:29 +0900

    ワーク

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :blogs
+  # get 'sessions/new'
+  root 'users#new'
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end

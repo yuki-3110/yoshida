fc45dd2bca5c3ebbc4f6e4b59ca4681285cee36a
yuki saito <ykst0622@gmail.com>
2022-06-22 17:44:48 +0900

    追加課題2完了

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      redirect_to blogs_path
+      redirect_to user_path(user.id)
     else
       flash[:danger] = 'ログインに失敗しました'
       render :new
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :blogs
   # get 'sessions/new'
   root 'users#new'
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :show]
   resources :sessions, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
